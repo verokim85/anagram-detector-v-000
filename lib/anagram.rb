1c5f756465b47decf68398d3eeabd50fb7b45ffa
Veronica Kim <vero.a.kim@gmail.com>
2018-02-19 16:59:04 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,8 +2,26 @@ class Anagram
 attr_accessor :word
 
 def match(arr_words)
+  x = []
+  z = []
 
-end
+  i = 0 
+    while i < arr_words.length
+      x << arr_words[i].split("")
+      i += 1
+  end 
+
+  num = 0 
+    while num < x.length 
+     z << x[num].sort
+    num += 1 
+  end 
+      z
+
+  if word != z 
+    return []
+end 
+
 
 
 end
