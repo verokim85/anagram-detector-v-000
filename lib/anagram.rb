84412d35bea169f56729c45fbce231f78a0e132f
Veronica Kim <vero.a.kim@gmail.com>
2018-02-19 18:04:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(arr_words)
+  new_arr = []
+
+  i = 0
+  while i < arr_words.length
+    if word.split("").sort == arr_words[i].split("").sort
+      new_arr << arr_words[i]
+        end
+          return new_arr
+    if word.split("").sort != arr_words[i].split("").sort
+      return []
+        end
+        i += 1
+    end
+  end
+end
