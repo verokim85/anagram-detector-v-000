adb792986ae42221624f1ac0bee908cb697da44b
Veronica Kim <vero.a.kim@gmail.com>
2018-02-19 16:59:19 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,23 +5,24 @@ def match(arr_words)
   x = []
   z = []
 
-  i = 0 
+  i = 0
     while i < arr_words.length
       x << arr_words[i].split("")
       i += 1
-  end 
+  end
 
-  num = 0 
-    while num < x.length 
+  num = 0
+    while num < x.length
      z << x[num].sort
-    num += 1 
-  end 
+    num += 1
+  end
       z
 
-  if word != z 
+  if word != z
     return []
-end 
 
+end
+end
 
 
 end
